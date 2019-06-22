edb16eb4961dfc6e2617ce8c08991ef52f7b2287
Waldemar Pankanin <biuro@kansoft.pl>
2019-06-22 12:42:53 +0200

    Updates

---
+++ precache-manifest.7287f5ffcc1aed2a5a05258345228b9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "963583ff992723a3cf3d6bb9cf961590",
+    "url": "/postbox/index.html"
+  },
+  {
+    "revision": "a810be785ed5090b9020",
+    "url": "/postbox/static/css/main.0c42a063.chunk.css"
+  },
+  {
+    "revision": "f3f19bed550e97472865",
+    "url": "/postbox/static/js/2.2ad71124.chunk.js"
+  },
+  {
+    "revision": "a810be785ed5090b9020",
+    "url": "/postbox/static/js/main.db4b1a79.chunk.js"
+  },
+  {
+    "revision": "822e0849053c19bfd42b",
+    "url": "/postbox/static/js/runtime~main.03cef49d.js"
+  },
+  {
+    "revision": "8332947445223f4da8b4169790e59d87",
+    "url": "/postbox/static/media/logo2.83329474.svg"
+  }
+]);
